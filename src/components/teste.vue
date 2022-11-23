c4dbb3d3a23e1e1e60f202a31a985e5cc120d88e
Alex Junior Barreto <alexjuniorbarreto@gmail.com>
2022-11-23 15:48:39 -0300

    update

--- src/components/teste.vue
+++ src/components/teste.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="burger-table">
+  <div id="burger-table" v-if="burgers">
     <div>
       <div id="burger-table-heading">
         <div class="order-id">#:</div>
@@ -17,10 +17,8 @@
         <div>{{ burger.pao }}</div>
         <div>{{ burger.carne }}</div>
         <div>
-          <ul>
-            <li v-for="(Opcional, index) in burger.opcionais" :key="index">
-              {{ Opcional }}
-            </li>
+          <ul v-for="(opcional, index) in burger.opcionais" :key="index">
+            <li>{{ opcional }}</li>
           </ul>
         </div>
         <div>
@@ -29,11 +27,10 @@
             class="status"
             @change="updateBurger($event, burger.id)"
           >
-            <option value="">Selecione</option>
             <option
+              :value="s.tipo"
               v-for="s in status"
               :key="s.id"
-              :value="s.tipo"
               :selected="burger.status == s.tipo"
             >
               {{ s.tipo }}
@@ -46,8 +43,10 @@
       </div>
     </div>
   </div>
+  <div v-else>
+    <h2>Não há pedidos no momento!</h2>
+  </div>
 </template>
-
 <script>
 export default {
   name: "Dashboard",
@@ -61,45 +60,32 @@ export default {
   methods: {
     async getPedidos() {
       const req = await fetch("http://localhost:3000/burgers");
-
       const data = await req.json();
-
       this.burgers = data;
-
-      //Resgatar os status
-
+      // Resgata os status de pedidos
       this.getStatus();
     },
     async getStatus() {
       const req = await fetch("http://localhost:3000/status");
-
       const data = await req.json();
-
       this.status = data;
     },
     async deleteBurger(id) {
       const req = await fetch(`http://localhost:3000/burgers/${id}`, {
         method: "DELETE",
       });
-
       const res = await req.json();
-
-      //msg
       this.getPedidos();
     },
-    async updatedPedidos(event, id) {
+    async updateBurger(event, id) {
       const option = event.target.value;
-
       const dataJson = JSON.stringify({ status: option });
-
       const req = await fetch(`http://localhost:3000/burgers/${id}`, {
         method: "PATCH",
         headers: { "Content-Type": "application/json" },
         body: dataJson,
       });
-
       const res = await req.json();
-
       console.log(res);
     },
   },
@@ -114,41 +100,34 @@ export default {
   max-width: 1200px;
   margin: 0 auto;
 }
-
 #burger-table-heading,
 #burger-table-rows,
 .burger-table-row {
   display: flex;
   flex-wrap: wrap;
 }
-
 #burger-table-heading {
   font-weight: bold;
   padding: 12px;
   border-bottom: 3px solid #333;
 }
-
-#burger-table-heading div,
-.burger-table-row div {
-  width: 19%;
-}
-
 .burger-table-row {
   width: 100%;
   padding: 12px;
   border-bottom: 1px solid #ccc;
 }
-
+#burger-table-heading div,
+.burger-table-row div {
+  width: 19%;
+}
 #burger-table-heading .order-id,
 .burger-table-row .order-number {
   width: 5%;
 }
-
 select {
   padding: 12px 6px;
   margin-right: 12px;
 }
-
 .delete-btn {
   background-color: #222;
   color: #fcba03;
